a2e850f908e4f88659072ef115be9239228d70cd
RadoKolev <rado.k.kolev@gmail.com>
2024-09-02 22:21:28 +0100

    Convex deployment error fix

--- src/providers/ConvexClerkProvider.tsx
+++ src/providers/ConvexClerkProvider.tsx
@@ -6,7 +6,7 @@ import { ConvexReactClient } from 'convex/react';
 import { ReactNode } from 'react';
 
 const convex = new ConvexReactClient(
-  process.env.NEXT_PUBLIC_CONVEX_URL as string
+  'https://gallant-toucan-563.convex.cloud'
 );
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
